36900e5e58e1c8107574c76408e1da90de5bf3c9
Eve <948832626@qq.com>
2021-04-04 14:11:52 +0800

    refactor: 删除navbar组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,17 @@ import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { NavbarComponent } from './navbar/navbar.component';
+import zh from '@angular/common/locales/zh';
 
 registerLocaleData(zh);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, BrowserAnimationsModule, HttpClientModule],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
